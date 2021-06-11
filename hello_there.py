8ef5d9a70673e86476a43bbe05cdd236e77367db
AdamBallard <72190282+AdamBallard@users.noreply.github.com>
2021-06-11 10:54:29 +0200

    Update hello_there.py

--- hello_there.py
+++ hello_there.py
@@ -4,7 +4,7 @@ import requests
 
 x = requests.get('www.google.se')
 
-  if(x.status_code == 200):
-    print('Status code is 200')
-    else:
-      print('Status code is not 200')
+if(x.status_code == 200):
+ print('Status code is 200')
+else:
+ print('Status code is not 200')
