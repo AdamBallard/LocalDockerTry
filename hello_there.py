ef0a316e86600d21a58908e48d050f0aa56b8d77
AdamBallard <72190282+AdamBallard@users.noreply.github.com>
2021-06-11 10:52:59 +0200

    Update hello_there.py

--- hello_there.py
+++ hello_there.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
-print('hello there!')
+import requests
 
+x = requests.get('www.google.se')
+
+  if(x.status_code == 200):
+    print('Status code is 200')
+    else:
+      print('Status code is not 200')
